c07e53d11ad8dfa5312dc0c2b7025efba71593fb
jxchew <jiaxian@crenergy.com.my>
2024-12-14 23:57:32 +0800

    Admin Push Notification

--- VolunteerRecruitmentPlatform/components/Header.js
+++ VolunteerRecruitmentPlatform/components/Header.js
@@ -21,7 +21,7 @@ function Header({ user }) {
                     <Text style={styles.userName}>{user.name}</Text>
                 </View>
             </View>
-            <TouchableOpacity onPress={() => navigation.navigate('Notification')}>
+            <TouchableOpacity onPress={() => navigation.navigate('PushNotification')}>
                 <Ionicons name="notifications-outline" size={24} color={Colors.black} />
             </TouchableOpacity>
         </View>
